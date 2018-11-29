8fdf29574f9dd5fad8fc25b0bfeba85b8cf8a074
Giovanny Rivera <eddyrivera@iMac-de-Eddy-Rivera.local>
2018-11-29 13:57:59 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-import React, { Component } from 'react';
-import './App.css';
-import Movie from './Movie'
+import React, { Component } from "react";
+import "./App.css";
+import Movie from "./Movie";
 class App extends Component {
-
   state = {
     movies: []
-  }  
+  };
 
-  componentDidMount(){
-    fetch('https://api.themoviedb.org/3/movie/popular?api_key=257889333e2a98d76029a6977fade69f&language=en-US&page=1')
-      .then( response => response.json())
-      .then( data => {
+  componentDidMount() {
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?api_key=257889333e2a98d76029a6977fade69f&language=en-US&page=1"
+    )
+      .then(response => response.json())
+      .then(data => {
         console.log(data);
         this.setState({
           movies: data.results
-        })
+        });
       });
   }
- 
-  render() {
 
+  render() {
     let mostrar = null;
 
-    if(this.state.movies.length === 0)
-      mostrar = <h1>Cargando</h1>
-    else
-      mostrar = this.state.movies.map(data => <Movie movie={data} />)
+    if (this.state.movies.length === 0) mostrar = <h1>Cargando</h1>;
+    else mostrar = this.state.movies.map(data => <Movie movie={data} />);
 
-    return (
-      <div className="App">
-        {mostrar}
-      </div>
-    );
+    return <div className="App">{mostrar}</div>;
   }
 }
 
